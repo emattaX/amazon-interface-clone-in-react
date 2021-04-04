3d9fc3ab7750dab56331147b9dcaa957a0382970
erne5t <ernestmatta@gmail.com>
2021-04-04 22:01:27 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDvnkr3GwO5ju0us_NyVauUhKaX5_WO85w",
+    authDomain: "react-clones-2699e.firebaseapp.com",
+    projectId: "react-clones-2699e",
+    storageBucket: "react-clones-2699e.appspot.com",
+    messagingSenderId: "410503656505",
+    appId: "1:410503656505:web:31412884a8fe23b42789e8"
+  };
